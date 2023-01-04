dffe92c1f251a12e9f0973367fbd20b79103e1c2
Tanweer Alam <tanweer.alam@tibilsolutions.com>
2023-01-04 12:48:56 +0530

    latest changes commit

--- js/main.js
+++ js/main.js
@@ -10,32 +10,35 @@
         }
     });
 
+
+
     $('.nav-item').on('click', function(e) {
         $(".navbar-collapse").collapse('hide');
       });
 
-    $(".navbar-toggler").on('click', function(e) {
-        $(".navbar-collapse").collapse('show');
-        // $('.nav-link').css('display', 'block'); 
-    });
+
+    // $(".navbar-toggler").on('click', function(e) {
+    //     $(".navbar-collapse").collapse('show');
+    //     // $('.nav-link').css('display', 'block'); 
+    // });
 
     
     // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
+    // $(document).ready(function () {
+    //     function toggleNavbarMethod() {
+    //         if ($(window).width() > 992) {
+    //             $('.navbar .dropdown').on('mouseover', function () {
+    //                 $('.dropdown-toggle', this).trigger('click');
+    //             }).on('mouseout', function () {
+    //                 $('.dropdown-toggle', this).trigger('click').blur();
+    //             });
+    //         } else {
+    //             $('.navbar .dropdown').off('mouseover').off('mouseout');
+    //         }
+    //     }
+    //     toggleNavbarMethod();
+    //     $(window).resize(toggleNavbarMethod);
+    // });
 
 
     // Modal Video
